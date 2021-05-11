85dc220ba2c0f4e05b7cc0c56ac84c0b6a9b4dd0
Nguyen duc duy <nguyenducduy1707@gmail.com>
2021-05-11 14:41:02 +0700

    complete search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,165 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Banner from "./components/Banner";
+import Search from "./components/Search";
 
 function App() {
+  const [offSet, setOffset] = useState();
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+    };
+  }, []);
+  const handleDisplaySearch = () => {
+    setIsShow(!isShow);
+  };
   return (
     <div className="App">
-      <Banner />
+      {offSet > 50 && (
+        <nav className="bg-white w-full flex flex-col py-4 border-b-2 sticky top-0 z-50">
+          <button
+            className="rounded-full mx-auto w-1/2 flex justify-between items-center space-x-5 focus:shadow-lg border focus:outline-none py-3 px-4"
+            onClick={handleDisplaySearch}
+          >
+            <span> Start your search</span>
+            <div className="bg-red-500 rounded-full p-2">
+              <svg
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+                style={{
+                  display: "block",
+                  fill: "none",
+                  height: "16px",
+                  width: "16px",
+                  stroke: "white",
+                  strokeWidth: "4",
+                  overflow: "visible",
+                }}
+              >
+                <g fill="none">
+                  <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+                </g>
+              </svg>
+            </div>
+          </button>
+          {isShow && <Search />}
+        </nav>
+      )}
+      <div className={`${offSet > 10 && "opacity-0"}`}>
+        <Search />
+      </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nam, nisi
+      id consequuntur temporibus est modi veniam ducimus totam aut error
+      voluptas necessitatibus eius! Dolores molestias repudiandae laborum
+      beatae? Iure? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus totam
+      aut error voluptas necessitatibus eius! Dolores molestias repudiandae
+      laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus
+      totam aut error voluptas necessitatibus eius! Dolores molestias
+      repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Dolore nam, nisi id consequuntur temporibus
+      est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius!pudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing pudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing pudiandae laborum beatae? Iure?Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolore nam, nisi id
+      consequuntur temporibus est modi veniam ducimus totam aut error voluptas
+      necessitatibus eius! Dolores molestias repudiandae laborum beatae?
+      Iure?Lorem ipsum dolor sit amet consectetur adipisicing pudiandae laborum
+      beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus totam
+      aut error voluptas necessitatibus eius! Dolores molestias repudiandae
+      laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing
+      pudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing pudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing pudiandae laborum beatae? Iure?Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolore nam, nisi id
+      consequuntur temporibus est modi veniam ducimus totam aut error voluptas
+      necessitatibus eius! Dolores molestias repudiandae laborum beatae?
+      Iure?Lorem ipsum dolor sit amet consectetur adipisicing pudiandae laborum
+      beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus totam
+      aut error voluptas necessitatibus eius! Dolores molestias repudiandae
+      laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing
+      pudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing Dolores molestias repudiandae laborum beatae?
+      Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nam,
+      nisi id consequuntur temporibus est modi veniam ducimus totam aut error
+      voluptas necessitatibus eius! Dolores molestias repudiandae laborum
+      beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus totam
+      aut error voluptas necessitatibus eius! Dolores molestias repudiandae
+      laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus
+      totam aut error voluptas necessitatibus eius! Dolores molestias
+      repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Dolore nam, nisi id consequuntur temporibus
+      est modi veniam ducimus totam aut error voluptas necessitatibus eius!
+      Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure? temporibus est
+      modi veniam ducimus totam aut error voluptas necessitatibus eius! Dolores
+      molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Dolore nam, nisi id consequuntur temporibus
+      est modi veniam ducimus totam aut error voluptas necessitatibus eius! D
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! D temporibus est modi veniam ducimus totam aut error voluptas
+      necessitatibus eius! Dolores molestias repudiandae laborum beatae?
+      Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nam,
+      nisi id consequuntur temporibus est modi veniam ducimus totam aut error
+      voluptas necessitatibus eius! D temporibus est modi veniam ducimus totam
+      aut error voluptas necessitatibus eius! Dolores molestias repudiandae
+      laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Dolore nam, nisi id consequuntur temporibus est modi veniam ducimus
+      totam aut error voluptas necessitatibus eius! D temporibus est modi veniam
+      ducimus totam aut error voluptas necessitatibus eius! Dolores molestias
+      repudiandae laborum beatae? Iure?Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolore nam, nisi id consequuntur temporibus est modi
+      veniam ducimus totam aut error voluptas necessitatibus eius! D temporibus
+      est modi veniam ducimus totam aut error voluptas necessitatibus eius!
+      Dolores molestias repudiandae laborum beatae? Iure?Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Dolore nam, nisi id consequuntur
+      temporibus est modi veniam ducimus totam aut error voluptas necessitatibus
+      eius! D temporibus est modi veniam ducimus totam aut error voluptas
+      necessitatibus eius! Dolores molestias repudiandae laborum beatae?
+      Iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nam,
+      nisi id consequuntur temporibus est modi veniam ducimus totam aut error
+      voluptas necessitatibus eius! D
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,54 @@
+function Search() {
+  return (
+    <div className="mt-20 flex mx-10 justify-center">
+      <div className="grid grid-cols-4 w-full max-w-2xl rounded-full border border-gray-200 cursor-pointer">
+        <div className="btn-effect btn-shadow">
+          <span className="text-left text-xs font-semibold">Location</span>
+          <input
+            type="text"
+            placeholder="Where are you going ?"
+            className="focus:outline-none overflow-ellipsis whitespace-nowrap overflow-hidden text-sm  placeholder-gray-500 bg-transparent"
+          />
+        </div>
+        <button className="btn-effect btn-shadow">
+          <span className="text-xs font-semibold">Check in</span>
+          <span className="text-sm text-gray-500">Add dates</span>
+        </button>
+        <button className="btn-effect btn-shadow">
+          <span className="text-xs font-semibold">Check out</span>
+          <span className="text-sm text-gray-500">Add dates</span>
+        </button>
+        <button className="flex justify-between pl-5 pr-2 py-3 text-left hover:bg-transparent rounded-full hover:bg-gray-100 focus-within:outline-none btn-shadow">
+          <div className="flex flex-col">
+            <span className="text-xs font-semibold">Guests</span>
+            <span className="text-sm text-gray-500">Add dates</span>
+          </div>
+          <div className="bg-red-500 rounded-full p-3">
+            <svg
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+              style={{
+                display: "block",
+                fill: "none",
+                height: "16px",
+                width: "16px",
+                stroke: "white",
+                strokeWidth: "4",
+                overflow: "visible",
+              }}
+            >
+              <g fill="none">
+                <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+              </g>
+            </svg>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
